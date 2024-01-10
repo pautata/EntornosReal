f00e0103af4acf1bdea480b18469182180aa86bd
paula_sobreroca <paula_sobreroca@1DAM-03>
2024-01-10 09:33:00 +0100

    triangulo

--- src/Entornos/EntornosReal.java
+++ src/Entornos/EntornosReal.java
@@ -1,10 +1,21 @@
 package Entornos;
 
+import java.util.Scanner;
 public class EntornosReal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Ingrese la base del triángulo:");
+       int base = scanner.nextInt();
+      
+       System.out.println("Ingrese la altura del triángulo:");
+       int altura = scanner.nextInt();
+       double areaTriangulo = calcularAreaTriangulo(base, altura);
+       System.out.println("El área del triángulo es: " + areaTriangulo);
+       scanner.close();
+   }
+   public static double calcularAreaTriangulo(int base, int altura) {
+       return (base * altura) / 2.0;
+     
+      
+   }
 }
